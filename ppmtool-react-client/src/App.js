47b56de99515e46890d8d326f03cec2d323829aa
Vitor Franco do Carmo <vfcarmo@gmail.com>
2019-12-05 20:32:23 +0000

    Bringing bootstrap 4+

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
