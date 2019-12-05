9966350a1e33f3ae804c52870802be0da940e3f6
Vitor Franco do Carmo <vfcarmo@gmail.com>
2019-12-05 20:13:06 +0000

    Project and header components

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import ProjectItem from "./project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
-    return <h1>Welcome to the Dashboard</h1>;
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>;
+        <ProjectItem />;
+      </div>
+    );
   }
 }
 
