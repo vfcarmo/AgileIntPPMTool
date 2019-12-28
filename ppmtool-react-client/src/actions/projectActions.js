700391561000b0e9de07350e77a6dac544ea942a
Vitor Franco do Carmo <vfcarmo@gmail.com>
2019-12-28 14:02:00 +0000

    Get Projects Redux

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,23 +1,42 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
   } catch (error) {
-    const err = {
-      projectName: "",
-      projectIdentifier: "",
-      description: ""
-    };
-    error.response.data.errors.map(error => {
-      const keys = error.title.split(" ");
-      err[keys[0]] = error.title;
-    });
     dispatch({
-      payload: err,
-      type: GET_ERRORS
+      type: GET_ERRORS,
+      payload: transformErrorToProjectError(error.response.data)
     });
   }
 };
+
+export const getProjects = () => async dispatch => {
+  try {
+    const res = await axios.get("http://localhost:8080/api/project");
+    dispatch({
+      type: GET_PROJECTS,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: transformErrorToProjectError(error.response.data)
+    });
+  }
+};
+
+function transformErrorToProjectError(data) {
+  const err = {
+    projectName: "",
+    projectIdentifier: "",
+    description: ""
+  };
+  data.errors.map(error => {
+    const keys = error.title.split(" ");
+    err[keys[0]] = error.title;
+  });
+  return err;
+}
