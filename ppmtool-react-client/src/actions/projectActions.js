eeedb2b945a0e7c3e3ff89e904b41c9ec2b914fe
Vitor Franco do Carmo <vfcarmo@gmail.com>
2019-12-30 19:40:05 +0000

    delete an existing project

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
@@ -64,6 +64,14 @@ export const updateProject = (project, history) => async dispatch => {
       payload: transformErrorToProjectError(error.response.data)
     });
   }
+};
+
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
 };
 
 function transformErrorToProjectError(data) {
